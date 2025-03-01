e55ca87c06eeace84aad8f3df4aac83e09fb7c48
Ianluck Rop <ianluckrp@ciencias.unam.mx>
2025-02-28 21:48:39 -0600

    Comentarios innecesarios eliminados

--- practicas/practica2/practica2.hs
+++ practicas/practica2/practica2.hs
@@ -11,23 +11,23 @@ count (Node _ left right) = 1 + count left + count right
 
 -- b) Función que cuenta la cantidad de niveles de profundidad del árbol
 height :: BinaryTree -> Int
-height Empty = 0 -- Height of a Empty Node.
-height (Node _ left right) = 1 + (max (height right) (height right)) -- Height of a non empty node
+height Empty = 0
+height (Node _ left right) = 1 + (max (height right) (height right))
 
 
--- c) Función que dado un entero no negativo z, devuelva un árbol binario balanceado1 que contenga
+-- c) Función que dado un entero no negativo z, devuelve un árbol binario balanceado que contiene
 --    en sus nodos a todos los enteros no negativos k ≤ z.
 balancedTree :: Int -> BinaryTree
 balancedTree z
   | z < 0     = Empty
   | otherwise = buildTree [0..z]
 
-  
+  -- Función auxiliar para construir el árbol binario balanceado.
 buildTree :: [Int] -> BinaryTree
 buildTree [] = Empty
 buildTree xs =
   let mid = length xs `div` 2
-      (left, root:right) = splitAt mid xs --Divide the list at the middle
+      (left, root:right) = splitAt mid xs
   in Node root (buildTree left) (buildTree right)
 
 
